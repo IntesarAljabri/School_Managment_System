3bd6fc4d7f9db927d623670f0df07243dcf027b3
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 11:44:36 +0400

    teacherControler

--- src/main/java/School_Managment_System/School_Managment_System/Controller/TeacherController.java
+++ src/main/java/School_Managment_System/School_Managment_System/Controller/TeacherController.java
@@ -1,4 +1,32 @@
 package School_Managment_System.School_Managment_System.Controller;
 
+import School_Managment_System.School_Managment_System.Model.Teacher;
+import School_Managment_System.School_Managment_System.Service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/teachers")
 public class TeacherController {
+    @Autowired
+     TeacherService teacherService;
+
+    @GetMapping
+    public ResponseEntity<List<Teacher>> getAllTeachers() {
+        List<Teacher> teachers = teacherService.getAllTeachers();
+        return ResponseEntity.ok(teachers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Teacher> getTeacherById(@PathVariable Long id) {
+        Optional<Teacher> teacher = teacherService.getTeacherById(id);
+        return teacher.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
 }
