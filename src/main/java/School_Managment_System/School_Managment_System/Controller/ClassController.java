21a4ee3d4e1e320eb30cd3c8570462e8ba3a5449
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 12:18:58 +0400

    ClassController

---
+++ src/main/java/School_Managment_System/School_Managment_System/Controller/ClassController.java
@@ -0,0 +1,9 @@
+package School_Managment_System.School_Managment_System.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/classes")
+public class ClassController {
+}
