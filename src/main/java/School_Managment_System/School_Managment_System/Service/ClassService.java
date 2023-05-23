507be0f1c894f648c6644f383421af48654f29f2
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 12:38:58 +0400

    ClassController

--- src/main/java/School_Managment_System/School_Managment_System/Service/ClassService.java
+++ src/main/java/School_Managment_System/School_Managment_System/Service/ClassService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.management.loading.ClassLoaderRepository;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,8 +13,8 @@ public class ClassService {
     @Autowired
     ClassLoaderRepository classRepository;
 
-    public List<Class> getAllClasses() {
-        return classRepository.finalize();
+    public List<Class> getAllClasses(Long id) {
+        return Collections.singletonList(classRepository.getClass());
     }
     public Optional<Class> getClassById(Long id) {
         return classRepository.findById(id);
