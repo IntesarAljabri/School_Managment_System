ddb9628b2fd5f4dbb7c8524ce4c1fbe066e03dc7
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 12:24:22 +0400

    ClassController

--- src/main/java/School_Managment_System/School_Managment_System/Controller/ClassController.java
+++ src/main/java/School_Managment_System/School_Managment_System/Controller/ClassController.java
@@ -2,8 +2,14 @@ package School_Managment_System.School_Managment_System.Controller;
 
 import School_Managment_System.School_Managment_System.Service.ClassService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/classes")
@@ -15,5 +21,10 @@ public class ClassController {
     @GetMapping
     public List<Class> getAllClasses() {
         return classService.getAllClasses();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Class> getClassById(@PathVariable Long id) {
+        Optional<Class> classOptional = classService.getClassById(id);
+
     }
 }
