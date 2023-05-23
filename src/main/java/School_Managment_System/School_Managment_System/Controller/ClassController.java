aff0d842cc18ee66bd7ef4f8b8ad9e8c17dd183b
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 12:20:04 +0400

    ClassController

--- src/main/java/School_Managment_System/School_Managment_System/Controller/ClassController.java
+++ src/main/java/School_Managment_System/School_Managment_System/Controller/ClassController.java
@@ -1,9 +1,15 @@
 package School_Managment_System.School_Managment_System.Controller;
 
+import School_Managment_System.School_Managment_System.Service.ClassService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/classes")
 public class ClassController {
+
+    @Autowired
+    ClassService classService;
+
 }
