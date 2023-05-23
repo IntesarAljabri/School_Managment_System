f9d35732d52e1d6620b361df492279e09454c64d
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 20:08:55 +0400

    Student_COurse

--- src/main/java/School_Managment_System/School_Managment_System/Model/Student_Course.java
+++ src/main/java/School_Managment_System/School_Managment_System/Model/Student_Course.java
@@ -1,4 +1,16 @@
 package School_Managment_System.School_Managment_System.Model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Setter
+@Getter
+@Table(name = "Student_Course")
 public class Student_Course {
 }
