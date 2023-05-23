6564eadd20cfa2460874976474bc46026d0c16eb
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 10:13:09 +0400

    studentClassModel

--- src/main/java/School_Managment_System/School_Managment_System/Model/Student.java
+++ src/main/java/School_Managment_System/School_Managment_System/Model/Student.java
@@ -1,4 +1,23 @@
 package School_Managment_System.School_Managment_System.Model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Data
+@Setter
+@Getter
+@Entity
+@Table(name = "Student")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String name;
+    String gender;
+    List <Course> Courses;
+
 }
