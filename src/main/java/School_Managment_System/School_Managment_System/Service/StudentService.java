169c00cf3117a7695197a2e17d8463f831248c7b
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 11:03:00 +0400

    StudentService

--- src/main/java/School_Managment_System/School_Managment_System/Service/StudentService.java
+++ src/main/java/School_Managment_System/School_Managment_System/Service/StudentService.java
@@ -1,5 +1,44 @@
 package School_Managment_System.School_Managment_System.Service;
 
+import School_Managment_System.School_Managment_System.Model.Course;
+import School_Managment_System.School_Managment_System.Model.Student;
+import School_Managment_System.School_Managment_System.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Optional<Student> getStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public List<Course> getCoursesByStudentId(Long id) {
+        Optional<Student> student = studentRepository.findById(id);
+        return student.map(Student::getCourses).orElse(Collections.emptyList());
+    }
 
 }
