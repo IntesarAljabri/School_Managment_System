01d69ea14789a7188ee797c7f699597f3560334d
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 11:23:08 +0400

    courseController

--- src/main/java/School_Managment_System/School_Managment_System/Controller/CourseController.java
+++ src/main/java/School_Managment_System/School_Managment_System/Controller/CourseController.java
@@ -6,4 +6,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        List<Course> courses = courseService.getAllCourses();
+        return ResponseEntity.ok(courses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+        Optional<Course> course = courseService.getCourseById(id);
+        return course.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        Course createdCourse = courseService.createCourse(course);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCourse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> updateCourse(@PathVariable Long id, @RequestBody Course course) {
+        Optional<Course> existingCourse = courseService.getCourseById(id);
+        if (existingCourse.isPresent()) {
+            course.setId(id);
+            Course updatedCourse = courseService.updateCourse(course);
+            return ResponseEntity.ok(updatedCourse);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
+        Optional<Course> existingCourse = courseService.getCourseById(id);
+        if (existingCourse.isPresent()) {
+            courseService.deleteCourse(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}/students")
+    public ResponseEntity<List<Student>> getStudentsByCourseId(@PathVariable Long id) {
+        List<Student> students = courseService.getStudentsByCourseId(id);
+        return ResponseEntity.ok(students);
+    }
 }
--- src/main/java/School_Managment_System/School_Managment_System/Service/TeacherService.java
+++ src/main/java/School_Managment_System/School_Managment_System/Service/TeacherService.java
@@ -1,4 +1,40 @@
 package School_Managment_System.School_Managment_System.Service;
 
+import School_Managment_System.School_Managment_System.Model.Teacher;
+import School_Managment_System.School_Managment_System.Repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TeacherService {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    public List<Teacher> getAllTeachers() {
+        return teacherRepository.findAll();
+    }
+
+    public Optional<Teacher> getTeacherById(Long id) {
+        return teacherRepository.findById(id);
+    }
+
+    public Teacher createTeacher(Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
+
+    public Teacher updateTeacher(Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
+
+    public void deleteTeacher(Long id) {
+        teacherRepository.deleteById(id);
+    }
+
+    public List<Teacher> getTeachersByCourse(String course) {
+        return teacherRepository.findByCourse(course);
+    }
 }
