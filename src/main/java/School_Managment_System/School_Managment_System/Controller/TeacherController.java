fabba82d7f87b58fb01f1d2956317f4a9c71a027
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 11:47:07 +0400

    teacherController

--- src/main/java/School_Managment_System/School_Managment_System/Controller/TeacherController.java
+++ src/main/java/School_Managment_System/School_Managment_System/Controller/TeacherController.java
@@ -3,11 +3,9 @@ package School_Managment_System.School_Managment_System.Controller;
 import School_Managment_System.School_Managment_System.Model.Teacher;
 import School_Managment_System.School_Managment_System.Service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +26,10 @@ public class TeacherController {
     public ResponseEntity<Teacher> getTeacherById(@PathVariable Long id) {
         Optional<Teacher> teacher = teacherService.getTeacherById(id);
         return teacher.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    public ResponseEntity<Teacher> createTeacher(@RequestBody Teacher teacher) {
+        Teacher createdTeacher = teacherService.createTeacher(teacher);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTeacher);
     }
 }
