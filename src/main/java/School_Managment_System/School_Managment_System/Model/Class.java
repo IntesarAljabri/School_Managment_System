0a46aa8a6038b39ca7ff0a9269954cee7845f2de
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 12:06:06 +0400

    ClassesModel AND RelationShip

--- src/main/java/School_Managment_System/School_Managment_System/Model/Class.java
+++ src/main/java/School_Managment_System/School_Managment_System/Model/Class.java
@@ -1,4 +1,27 @@
 package School_Managment_System.School_Managment_System.Model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Setter
+@Getter
+@Entity
+@Table(name = "Class")
 public class Class {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    Integer size;
+    String classCode;
+
+    // Define the relationship with Student
+    @OneToMany(mappedBy = "class")
+    private List<Student> students;
+
 }
--- src/main/java/School_Managment_System/School_Managment_System/Model/Course.java
+++ src/main/java/School_Managment_System/School_Managment_System/Model/Course.java
@@ -28,6 +28,6 @@ public class Course {
     @OneToMany(mappedBy = "course")
     private List<Teacher> teachers;
 
- 
+
 
 }
