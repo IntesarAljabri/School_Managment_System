f4be5efca8c1826bca630232b38bda90fa8733d6
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 10:50:01 +0400

    Course

---
+++ src/main/java/School_Managment_System/School_Managment_System/Repository/CourseRepository.java
@@ -0,0 +1,4 @@
+package School_Managment_System.School_Managment_System.Repository;
+
+public interface CourseRepository {
+}
--- src/main/java/School_Managment_System/School_Managment_System/Service/CourseService.java
+++ src/main/java/School_Managment_System/School_Managment_System/Service/CourseService.java
@@ -1,4 +1,31 @@
 package School_Managment_System.School_Managment_System.Service;
 
+import School_Managment_System.School_Managment_System.Model.Course;
+import School_Managment_System.School_Managment_System.Repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CourseService {
+    @Autowired
+    CourseRepository courseRepository;
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public Optional<Course> getCourseById(Long courseId) {
+        return courseRepository.findById(courseId);
+    }
+
+    public void deleteCourse(Long courseId) {
+        courseRepository.deleteById(courseId);
+    }
 }
