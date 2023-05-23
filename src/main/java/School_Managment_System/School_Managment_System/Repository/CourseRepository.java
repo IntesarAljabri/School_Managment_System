dbe077ffba7697a85c3eda6c86149c225317049c
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-23 10:52:20 +0400

    CourseRepository

--- src/main/java/School_Managment_System/School_Managment_System/Repository/CourseRepository.java
+++ src/main/java/School_Managment_System/School_Managment_System/Repository/CourseRepository.java
@@ -1,4 +1,7 @@
 package School_Managment_System.School_Managment_System.Repository;
 
-public interface CourseRepository {
+import School_Managment_System.School_Managment_System.Model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
 }
