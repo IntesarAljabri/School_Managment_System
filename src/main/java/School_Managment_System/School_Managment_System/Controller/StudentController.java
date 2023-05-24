5d08d518fcc47cab364a6a35db78c27d7c14eca8
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-24 12:01:59 +0400

    Student

--- src/main/java/School_Managment_System/School_Managment_System/Controller/StudentController.java
+++ src/main/java/School_Managment_System/School_Managment_System/Controller/StudentController.java
@@ -2,13 +2,8 @@ package School_Managment_System.School_Managment_System.Controller;
 
 import School_Managment_System.School_Managment_System.Model.Course;
 import School_Managment_System.School_Managment_System.Model.Student;
-import School_Managment_System.School_Managment_System.Repository.StudentRepository;
-import School_Managment_System.School_Managment_System.Service.ClassService;
-import School_Managment_System.School_Managment_System.Service.CourseService;
 import School_Managment_System.School_Managment_System.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,13 +22,13 @@ public class StudentController {
     }
 
     @GetMapping(value = "getById")
-    public Course getClassById(Long id) {
-        return courseService.getCourseById(id);
+    public Student getStudentById(Long id) {
+        return studentService.getStudentById(id);
     }
 
     @PostMapping(value = "add")
-    public String Course(@RequestBody Course courses) {
-        courseService.addCourse(courses);
+    public String Student(@RequestBody Student students) {
+        studentService.addStudent(students);
         return "Class added";
     }
 }
