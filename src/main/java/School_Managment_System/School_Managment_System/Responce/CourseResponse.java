89f56b008fa70e4f0bf656713733f094dbde46fe
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-25 14:41:32 +0400

    CourseResponse

--- src/main/java/School_Managment_System/School_Managment_System/Responce/CourseResponse.java
+++ src/main/java/School_Managment_System/School_Managment_System/Responce/CourseResponse.java
@@ -1,4 +1,27 @@
 package School_Managment_System.School_Managment_System.Responce;
 
+import School_Managment_System.School_Managment_System.Model.Course;
+import School_Managment_System.School_Managment_System.Model.Student;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourseResponse {
+
+
+    private Long id;
+    private String course_name;
+    private String description;
+    private String code;
+
+    public Course convertToCourse () {
+        Date nowDate = new Date();
+      
 }
