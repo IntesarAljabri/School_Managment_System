db382f9bc0249dee87f463f3abc584eb579b0224
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-25 15:42:25 +0400

    ClassResponse

--- src/main/java/School_Managment_System/School_Managment_System/Responce/ClassResponse.java
+++ src/main/java/School_Managment_System/School_Managment_System/Responce/ClassResponse.java
@@ -1,4 +1,13 @@
 package School_Managment_System.School_Managment_System.Responce;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClassResponse {
 }
