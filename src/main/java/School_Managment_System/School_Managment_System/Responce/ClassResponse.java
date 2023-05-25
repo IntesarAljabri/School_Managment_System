f6e6efaf3644a35b9d75f48e7eb8478b12170764
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-25 15:46:31 +0400

    ClassResponse

--- src/main/java/School_Managment_System/School_Managment_System/Responce/ClassResponse.java
+++ src/main/java/School_Managment_System/School_Managment_System/Responce/ClassResponse.java
@@ -1,13 +1,28 @@
 package School_Managment_System.School_Managment_System.Responce;
 
+import School_Managment_System.School_Managment_System.Model.Classroom;
+import School_Managment_System.School_Managment_System.Model.Course;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.OneToOne;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClassResponse {
+
+    private Long id;
+    private String name;
+    private String phone;
+    private String specialization;
+    private String age;
+
+    public Classroom convertToClass () {
+        
+    }
+
 }
