36b34df9858f82a1a638a1fc6253b5cb7ac7efd6
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-25 15:58:10 +0400

    ClassRequest

---
+++ src/main/java/School_Managment_System/School_Managment_System/Request/ClassRequest.java
@@ -0,0 +1,13 @@
+package School_Managment_System.School_Managment_System.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClassRequest {
+}
