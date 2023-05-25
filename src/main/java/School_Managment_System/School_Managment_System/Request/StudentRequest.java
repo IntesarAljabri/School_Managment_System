0c713b48ec6d66b301015813fe2734efad2130b2
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-25 11:44:15 +0400

    StudentResponce

--- src/main/java/School_Managment_System/School_Managment_System/Request/StudentRequest.java
+++ src/main/java/School_Managment_System/School_Managment_System/Request/StudentRequest.java
@@ -1,4 +1,21 @@
 package School_Managment_System.School_Managment_System.Request;
 
-public class StudentRequest {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@PostMapping("/addStudent")
+public class StudentRequest addStudent (@RequestBody StudentRequest studentRequest) {
+
+        Student savedStudent = studentServices.addStudent(studentRequest.convertToStudent());
+
+        StudentResponse response = new StudentResponse(
+                savedStudent.getId(),
+                savedStudent.getName(),
+                savedStudent.getAge(),
+                savedStudent.getGender(),
+                savedStudent.getEmail(),
+                savedStudent.getCreatedDate());
+        return response;
+
+
 }
