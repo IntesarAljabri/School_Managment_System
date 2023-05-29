913c0ca43c321922529e43154b1ff414f2359ee4
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-29 09:24:20 +0400

    StudentRepository

--- src/main/java/School_Managment_System/School_Managment_System/Repository/StudentRepository.java
+++ src/main/java/School_Managment_System/School_Managment_System/Repository/StudentRepository.java
@@ -1,7 +1,13 @@
 package School_Managment_System.School_Managment_System.Repository;
+import School_Managment_System.School_Managment_System.Model.Course;
 import School_Managment_System.School_Managment_System.Model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Student> findAllByisActive(boolean active);
 }
