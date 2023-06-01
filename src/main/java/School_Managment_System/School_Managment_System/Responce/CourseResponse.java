64d411e27c9da0a392eb008d1c414931679fb2f3
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-01 10:25:28 +0400

    CourseResponse

--- src/main/java/School_Managment_System/School_Managment_System/Responce/CourseResponse.java
+++ src/main/java/School_Managment_System/School_Managment_System/Responce/CourseResponse.java
@@ -2,17 +2,17 @@ package School_Managment_System.School_Managment_System.Responce;
 
 import School_Managment_System.School_Managment_System.Model.Course;
 import School_Managment_System.School_Managment_System.Model.Student;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CourseResponse {
 
 
@@ -21,6 +21,15 @@ public class CourseResponse {
     private String description;
     private String code;
 
+    public static CourseResponse convertRequestToResponse(Course courseRequest) {
+        return CourseResponse.builder()
+                .id(courseRequest.getId())
+                .course_name((courseRequest.getCourse_name()))
+                .description(courseRequest.getDescription())
+                .code(courseRequest.getCode())
+                .build();
+    }
 
+    
 
 }
