5747584f0050770e84750de6fb0d1a5d6d4d5437
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-01 09:52:49 +0400

    CourseRepository

--- src/main/java/School_Managment_System/School_Managment_System/Repository/CourseRepository.java
+++ src/main/java/School_Managment_System/School_Managment_System/Repository/CourseRepository.java
@@ -2,6 +2,8 @@ package School_Managment_System.School_Managment_System.Repository;
 import School_Managment_System.School_Managment_System.Model.Course;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.metamodel.SingularAttribute;
@@ -12,5 +14,11 @@ import java.util.Optional;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> findAllByisActive(boolean active);
+
+    @Query(value = "select c from Course c")
+    List<Course> getAllCourses();
+
+    @Query(value = "select c from Course c where c.id = :courseId")
+    Course getCourseById(@Param("courseId") Long id);
 
 }
