6a29519b6ef5772c8902fdf25410699ffbf2917d
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-05 15:39:39 +0400

    CourseResponse missing } in closed

--- src/main/java/School_Managment_System/School_Managment_System/Responce/StudentResponse.java
+++ src/main/java/School_Managment_System/School_Managment_System/Responce/StudentResponse.java
@@ -2,10 +2,7 @@ package School_Managment_System.School_Managment_System.Responce;
 
 import School_Managment_System.School_Managment_System.Model.Student;
 import School_Managment_System.School_Managment_System.Request.StudentRequest;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +12,7 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class StudentResponse {
     private Long id;
     private String name;
@@ -22,7 +20,14 @@ public class StudentResponse {
     private String gender;
     private String email;
 
+    public static StudentResponse convertRequestToResponse(Student studentRequest) {
+        return StudentResponse.builder()
+                .id(studentRequest.getId())
+                .name(studentRequest.getName())
+                .gender(studentRequest.getGender())
+                .age(studentRequest.getAge())
+                .email(studentRequest.getEmail())
+                .build();
 
-
-
+    }
 }
