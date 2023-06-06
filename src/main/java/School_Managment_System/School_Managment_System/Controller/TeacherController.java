3fc831c83933a7339bbfd698af0d064a1dc1992a
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-06 09:26:26 +0400

    Wite the command in TeacherController

--- src/main/java/School_Managment_System/School_Managment_System/Controller/TeacherController.java
+++ src/main/java/School_Managment_System/School_Managment_System/Controller/TeacherController.java
@@ -1,22 +1,14 @@
 package School_Managment_System.School_Managment_System.Controller;
 
-import School_Managment_System.School_Managment_System.Model.Course;
-import School_Managment_System.School_Managment_System.Model.Student;
 import School_Managment_System.School_Managment_System.Model.Teacher;
-import School_Managment_System.School_Managment_System.Request.StudentRequest;
 import School_Managment_System.School_Managment_System.Request.TeacherRequest;
-import School_Managment_System.School_Managment_System.Responce.StudentResponse;
 import School_Managment_System.School_Managment_System.Responce.TeacherResponse;
-import School_Managment_System.School_Managment_System.Service.CourseService;
-import School_Managment_System.School_Managment_System.Service.StudentService;
 import School_Managment_System.School_Managment_System.Service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/teachers")
@@ -24,43 +16,44 @@ public class TeacherController {
     @Autowired
     TeacherService teacherService;
 
+    //Get all Teacher
     @GetMapping(value = "getAll")
     public List<Teacher> getAllTeacher() {
         return teacherService.getAllTeachers();
     }
 
+    //Get by id
     @GetMapping(value = "getById")
     public Teacher getTeacherById(@RequestParam Long id) {
         return teacherService.getTeacherById(id);
     }
 
-//    @PostMapping(value = "add")
-//    public String Teacher(@RequestBody Teacher teacher) {
-//        TeacherService.addTeachers(teacher);
-//        return "Class added";
-//    }
 
-    @PostMapping("/addTeacher")
-    public TeacherResponse addTeacher(@RequestBody TeacherRequest teacherRequest) {
-        Teacher savedTeacher = teacherService.addTeacher(teacherRequest.convertToTeacher());
+    //Add new teacher
+   @PostMapping("/addTeacher")
+  public TeacherResponse addTeacher(@RequestBody TeacherRequest teacherRequest) {
+    Teacher savedTeacher = teacherService.addTeacher(teacherRequest.convertToTeacher());
 
-        TeacherResponse response = new TeacherResponse(
-                savedTeacher.getId(),
-                savedTeacher.getName(),
-                savedTeacher.getPhone(),
-                savedTeacher.getSpecialization(),
-                savedTeacher.getAge()
-        );
+    TeacherResponse response = new TeacherResponse(
+            savedTeacher.getId(),
+            savedTeacher.getName(),
+            savedTeacher.getPhone(),
+            savedTeacher.getSpecialization(),
+            savedTeacher.getAge()
+    );
 
-        return response;
-    }
+    return response;
+}
 
+  //delete teacher by id
     @DeleteMapping("/{id}")
     public String deleteTeacher(@PathVariable Long id) {
         teacherService.deleteTeacher(id);
         return teacherService.deleteTeacher(id);
     }
 
+
+   //Update teacher information
     @PutMapping("{id}")
     public ResponseEntity<Teacher> updateTeacher(@PathVariable Long id, @RequestBody Teacher updateData){
         Teacher teachers = teacherService.updateTeacher(id , updateData);
